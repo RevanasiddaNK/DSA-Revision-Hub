1a2b9da0adcecdff458d5dd968a581580b8cdd6d
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2024-10-23 00:46:15 +0530

    Time: 20 ms (8.05%), Space: 19.1 MB (7.77%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<string>roman = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+       
         vector<string> thousands = { "", "M", "MM", "MMM" };
         vector<string> hundreds = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
         vector<string> tens = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
         vector<string> units = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
 
         string romanAns1 = ( thousands[num/1000] + hundreds[ (num%1000)/100 ] + tens[ (num%100)/10 ] + units[num%10]  );
-        return romanAns1;
+        //return romanAns1;
+
+        vector<string>roman = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        vector<int>values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string romanAns2 = "";
+        for(int i=0; i<13; i++){
+
+          
+                int repTimes = num / values[i];
+                while(repTimes){
+                    romanAns2 += roman[i];
+                    repTimes--;
+                }
+
+                num = num %  values[i];
+           
+        }
+
+        return romanAns2;
 
     }
 };
