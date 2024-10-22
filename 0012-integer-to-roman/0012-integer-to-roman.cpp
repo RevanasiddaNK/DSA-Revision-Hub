37ae6dfbd75164b985119379c8c41aa79b3f7c9a
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2024-10-23 00:35:36 +0530

    Time: 11 ms (31.85%), Space: 18.7 MB (7.77%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -7,8 +7,8 @@ public:
         vector<string> tens = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
         vector<string> units = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
 
-
-        return ( thousands[num/1000] + hundreds[ (num%1000)/100 ] + tens[ (num%100)/10 ] + units[num%10]  );
+        string romanAns1 = ( thousands[num/1000] + hundreds[ (num%1000)/100 ] + tens[ (num%100)/10 ] + units[num%10]  );
+        return romanAns1;
 
     }
 };
