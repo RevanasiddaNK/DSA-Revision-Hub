61f754b09252c68467b257fa69a706e5a13520eb
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2024-11-02 12:04:56 +0530

    Time: 2 ms (71.83%), Space: 11.9 MB (30.4%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        stack<string> st;
+        stack<string>st;
         string curr = "";
-
-        for (char ch : path) {
-            if (ch == '/') {
-                if (curr == "..") {
-                    if (!st.empty()) 
-                        st.pop();
-                }
-                else if (!curr.empty() && curr != ".") {
-                    st.push(curr); 
-                }
-                curr = "";
+        for(char ch : path){
+            
+            if(ch == '/'){
+                if(curr == ".." && !st.empty())
+                    st.pop();
+                else if( !curr.empty() && curr != "." && curr != "..")
+                    st.push(curr);
+                
+                curr = ""; 
             }
-            else {
+            else
                 curr += ch;
-            }
         }
 
-        // Handle any remaining directory after the loop
-        if (curr == ".." && !st.empty()) 
+        if(curr == ".."  && !st.empty()){
             st.pop();
-        else if (!curr.empty() && curr != ".") 
+        }
+        else if (!curr.empty() && curr != "." && curr != "..") {
             st.push(curr);
-
-        // Construct simplified path
+        }
+    
         string ans = "";
-        while (!st.empty()) {
-            if(st.top() != ".."){
-                ans = "/" + st.top() + ans;
-            }
+
+        while(!st.empty()){
+            ans = "/" + st.top() + ans;
             st.pop();
         }
 
         return ans.empty() ? "/" : ans;
+
     }
 };
-
-/*
-TC : O(N)
-SC : O(N)
-*/
