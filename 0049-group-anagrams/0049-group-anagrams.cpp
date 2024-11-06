a1800c94ebffc42c4493900cb4a5489c013b2333
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2024-11-06 18:02:42 +0530

    Time: 17 ms (67.01%), Space: 25.7 MB (26.05%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    vector<vector<string>> groupAnagramsUsingHashMap(vector<string>& strs) {
+        unordered_map<string, vector<string> >mp;
+        vector<string>anagrams;
+        int n = strs.size();
+        for(int i=0; i<n ; i++){
+            string temp = strs[i];
+            sort(temp.begin(), temp.end());
+            if(mp.find(temp) != mp.end()){
+                mp[temp].push_back(strs[i]);
+            }
+            else{
+                mp[temp].push_back(strs[i]);
+                anagrams.push_back(temp);
+            }
+        }
+        vector<vector<string>>ans;
+        for(int i=0; i<anagrams.size(); i++){
+            ans.push_back(mp[anagrams[i]]);
+        }
+
+        return ans;
+
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        return groupAnagramsUsingHashMap(strs);
+    }
+};
