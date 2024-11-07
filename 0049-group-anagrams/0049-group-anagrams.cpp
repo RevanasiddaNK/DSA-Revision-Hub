257b851376c2602aa3204a148e3e3deb136b07cb
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2024-11-07 12:49:22 +0530

    Time: 24 ms (33.48%), Space: 25.6 MB (26.57%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
-
-        for (const string& str : strs) {
-            // Create a frequency-based key
-            vector<int> freq(26, 0);
-            for (char ch : str) {
-                freq[ch - 'a']++;
+        unordered_map<string, vector<string> >mp;
+        vector<string>anagrams;
+        int n = strs.size();
+        for(int i=0; i<n ; i++){
+            string temp = strs[i];
+            sort(temp.begin(), temp.end());
+            if(mp.find(temp) != mp.end()){
+                mp[temp].push_back(strs[i]);
             }
-
-            // Convert frequency array to a unique string key
-            string key;
-            for (int count : freq) {
-                key += '#' + to_string(count);
+            else{
+                mp[temp].push_back(strs[i]);
+                anagrams.push_back(temp);
             }
-
-            // Group the original string using this frequency key
-            mp[key].push_back(str);
         }
-
-        // Collect all anagram groups from the hashmap
-        vector<vector<string>> ans;
-        for (const auto& entry : mp) {
-            ans.push_back(entry.second);
+        vector<vector<string>>ans;
+        for(int i=0; i<anagrams.size(); i++){
+            ans.push_back(mp[anagrams[i]]);
         }
 
         return ans;
+
     }
 };
-/*
-Complexity Analysis
-    Time Complexity: O(N×K)
-    Space Complexity: O(N×K)
-*/
