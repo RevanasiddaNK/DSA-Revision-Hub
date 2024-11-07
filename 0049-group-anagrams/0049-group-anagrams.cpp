66d4f441cef7c91006d0132a727699418415df95
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2024-11-07 12:40:48 +0530

    Time: 66 ms (13.81%), Space: 33.5 MB (8.86%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
 
-    vector<vector<string>> groupAnagramsUsingHashMap(vector<string>& strs) {
-        unordered_map<string, vector<string> >mp;
-        vector<string>anagrams;
-        int n = strs.size();
-        for(int i=0; i<n ; i++){
-            string temp = strs[i];
-            sort(temp.begin(), temp.end());
-            if(mp.find(temp) != mp.end()){
-                mp[temp].push_back(strs[i]);
+        for (const string& str : strs) {
+            // Create a frequency-based key
+            vector<int> freq(26, 0);
+            for (char ch : str) {
+                freq[ch - 'a']++;
             }
-            else{
-                mp[temp].push_back(strs[i]);
-                anagrams.push_back(temp);
+
+            // Convert frequency array to a unique string key
+            string key;
+            for (int count : freq) {
+                key += '#' + to_string(count);
             }
+
+            // Group the original string using this frequency key
+            mp[key].push_back(str);
         }
-        vector<vector<string>>ans;
-        for(int i=0; i<anagrams.size(); i++){
-            ans.push_back(mp[anagrams[i]]);
+
+        // Collect all anagram groups from the hashmap
+        vector<vector<string>> ans;
+        for (const auto& entry : mp) {
+            ans.push_back(entry.second);
         }
 
         return ans;
-
-    }
-
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        return groupAnagramsUsingHashMap(strs);
     }
 };
+/*
+Complexity Analysis
+    Time Complexity: O(N×K)
+    Space Complexity: O(N×K)
+*/
