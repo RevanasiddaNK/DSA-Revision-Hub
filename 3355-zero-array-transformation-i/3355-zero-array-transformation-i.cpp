05686df72c7c1068b79d9620d08caedcf5c61c4f
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2024-11-17 21:29:05 +0530

    Time: 0 ms (100%), Space: 291.9 MB (100%) - LeetHub

---
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+//  solved using Line Sweep Algorithm
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>&queries) {
+        int n = nums.size();
+        vector<int>sweep(n, 0);
+
+        for(int i=0; i< queries.size(); i++){
+            sweep[ queries[i][0] ]++;
+            if( queries[i][1] + 1 < n ){
+                sweep[ queries[i][1] + 1]--;
+            }
+        }
+
+        int sum = 0;
+        for(int i=0; i< sweep.size(); i++){
+            sum += sweep[i];
+            sweep[i] = sum;
+        }
+
+        for(int i=0; i< sweep.size(); i++){
+            if(sweep[i] < nums[i])
+                return 0;
+        }
+        return 1;
+
+    }
+};
+
+/*
+Final Complexity: O(N)
+Time Complexity: O(1)
+*/
