e5e0553973189207fd041f00d3210c4261170ef3
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2024-12-22 19:58:47 +0530

    Time: 8 ms (52.51%), Space: 15.3 MB (20.16%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -2,54 +2,56 @@ class Solution {
 public:
     int calculate(string s) {
         char op = '+';
-        stack<int>st;
+        stack<int> st;
 
-        for(int i=0; i<s.size(); i++){
+        for (int i = 0; i < s.size(); i++) {
 
-            if(iswspace(s[i]))
+            if (iswspace(s[i]))
                 continue;
 
-            if(isdigit(s[i])){
+            if (isdigit(s[i])) {
                 int n = 0;
-                while(i < s.size() && isdigit(s[i])){
+                while (i < s.size() && isdigit(s[i])) {
                     int n2 = s[i] - '0';
                     n *= 10;
                     n += n2;
                     i++;
                 }
                 i--;
-               
-                if(op == '+'){
+
+                if (op == '+') {
                     st.push(n);
-                    n=0;
-                }
-                else if(op == '-'){
-                    st.push(-1*n);
                     n = 0;
-                }
-                else if(!st.empty() && op == '*' ){
+                } else if (op == '-') {
+                    st.push(-1 * n);
+                    n = 0;
+                } else if (!st.empty() && op == '*') {
                     int n1 = st.top();
                     st.pop();
-                    st.push(n*n1);
+                    st.push(n * n1);
                     n = 0;
-                }
-                else if(!st.empty() && op == '/' ){
+                } else if (!st.empty() && op == '/') {
                     int n1 = st.top();
                     st.pop();
-                    st.push(n1/n);
-                    n=0;
+                    st.push(n1 / n);
+                    n = 0;
                 }
-            }
-            else{
+            } else {
                 op = s[i];
             }
         }
 
         int ans = 0;
-        while(!st.empty()){
+        while (!st.empty()) {
             ans += st.top();
             st.pop();
         }
         return ans;
     }
 };
+/*
+Time Complexity: 
+O(n)
+Space Complexity:
+O(n)
+*/
