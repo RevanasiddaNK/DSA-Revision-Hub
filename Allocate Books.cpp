ea31e9b353fdb83e5019efec6af91124ddf1e788
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-03-11 10:17:13 +0530

    Update Allocate Books.cpp

--- Allocate Books.cpp
+++ Allocate Books.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 // Function to check if we can allocate within `maxTime`
 bool isPossible(int n, vector<int>& time, long long maxTime) {
-    int daysUsed = 1;  // Start from 1st day
-    long long dailyTime = 0;  // Accumulated time for current day
+    int daysUsed = n; 
+    long long dailyTime = 0;  
 
     for (int i = 0; i < time.size(); i++) {
         if (dailyTime + time[i] > maxTime) {  
-            // Move to the next day
-            daysUsed++;
+            
+            daysUsed--;
             dailyTime = time[i];
 
-            if (daysUsed > n) return false; // More days needed than available
+            if (daysUsed <=  0) return false;
         } else {
-            dailyTime += time[i];  // Accumulate study time in current day
+            dailyTime += time[i];
         }
     }
     return true;
@@ -29,21 +29,13 @@ long long ayushGivesNinjatest(int n, int m, vector<int> time) {
         long long mid = start + (end - start) / 2;
 
         if (isPossible(n, time, mid)) {
-            ans = mid;  // Store the answer
-            end = mid - 1; // Try for a smaller maxTime
+            ans = mid;  
+            end = mid - 1; 
         } else {
-            start = mid + 1; // Increase maxTime
+            start = mid + 1; 
         }
     }
     return ans;
 }
 
-// Driver code
-int main() {
-    int n = 3, m = 5;
-    vector<int> time = {10, 20, 30, 40, 50};  // Example input
-    cout << "Minimal max time Ayush studies in a day: " << ayushGivesNinjatest(n, m, time) << endl;
-    return 0;
-}
 
-//  https://www.naukri.com/code360/problems/ayush-and-ninja-test_1097574
