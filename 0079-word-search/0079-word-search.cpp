f8fb3b2784f372583365727d52c7bc70a0cf97a8
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-03-12 10:51:41 +0530

    Time: 195 ms (86.03%), Space: 10.4 MB (87.04%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
+    bool solve(vector<vector<char>>& board, string &word, int i, int j, int index) {
+        // Base case: If we found the entire word
+        if (index == word.size()) {
+            return true;
+        }
 
-    bool  search(int i, int j, int index,  vector<vector<char>>& board, string word) {
+        // Boundary checks and character match
+        if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] != word[index]) {
+            return false;
+        }
 
-            if(index ==  word.size()-1)
-                return 1;
-            
-            if(i<0 || i>=board.size() || j<0 || j >= board[0].size() )
-                return 0;
+        // Mark cell as visited
+        char ch = board[i][j];
+        board[i][j] = '*';
 
-            char temp = board[i][j];
-            board[i][j] = '*';
+        // Explore all 4 directions
+        bool found = solve(board, word, i - 1, j, index + 1) ||  // Up
+                     solve(board, word, i + 1, j, index + 1) ||  // Down
+                     solve(board, word, i, j - 1, index + 1) ||  // Left
+                     solve(board, word, i, j + 1, index + 1);    // Right
 
-            bool up=0,down=0,left=0, right=0;
-            if(j-1 >= 0 && board[i][j-1] == word[index+1])
-                left = search(i, j-1, index+1,  board, word );
+        // Restore cell (Backtracking)
+        board[i][j] = ch;
 
-            if( j+1 < board[0].size() && board[i][j+1] == word[index+1])
-                right = search(i, j+1, index+1,  board, word );
-
-            if(i-1 >= 0 && board[i-1][j] == word[index+1])
-                up = search(i-1, j, index+1,  board, word );
-            
-            if( i+1 < board.size() && board[i+1][j] == word[index+1])
-                down = search(i+1, j, index+1,  board, word );
-            
-            board[i][j] = temp;
-
-          return up || down || left || right;
+        return found;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
-        
-        for(int i=0; i<board.size(); i++ ){
-            for(int j=0; j<board[0].size(); j++){
-                if(board[i][j] == word[0]){
-                    if( search(i, j, 0,  board, word ) )
-                        return 1;
+        int m = board.size(), n = board[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // Start DFS from every matching character
+                if (board[i][j] == word[0] && solve(board, word, i, j, 0)) {
+                    return true;
                 }
             }
         }
-        return 0;
+        return false;
     }
 };
