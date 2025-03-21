2c11caa87ed764f89340ba5c37919e25b69946ee
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-03-21 11:41:29 +0530

    Time: 0 ms (100%), Space: 12.8 MB (47.4%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
 
-    bool isSameTree(TreeNode* p, TreeNode* q) {
+    bool DFSSolution(TreeNode* p, TreeNode* q) {
         if(p == NULL && q == NULL){
             return 1;
         }else if(p == NULL || q == NULL){
@@ -20,9 +20,16 @@ public:
         }
         if(p->val != q->val)
             return 0;
-        bool l = isSameTree(p->left, q->left);
-        bool r = isSameTree(p->right, q->right);
+        bool l = DFSSolution(p->left, q->left);
+        bool r = DFSSolution(p->right, q->right);
 
         return l&&r;
+    }
+
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        bool ans = false;
+        ans =  DFSSolution(p,q);
+
+        return ans;
     }
 };
