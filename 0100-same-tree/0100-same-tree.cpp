d3b4f539636be48f30659ddfc98365374b7dd2a9
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-03-21 11:57:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -26,9 +26,50 @@ public:
         return l&&r;
     }
 
+    bool BFSSolution(TreeNode* p, TreeNode* q) {
+        queue<TreeNode*>q1;
+        queue<TreeNode*>q2;
+        q1.push(p);
+        q2.push(q);
+
+        while(! (q1.empty() || q2.empty() ) ){
+            TreeNode* c1 = q1.front();
+            TreeNode* c2 = q2.front();
+            if(c1->val != c2->val)
+                return 0;
+           
+            q1.pop();
+            q2.pop();
+
+            if(c1->left && c2->left) {
+                q1.push(c1->left);
+                q2.push(c2->left);
+            } 
+            else if(c1->left || c2->left  ){
+                return 0;
+            }
+            if(c1->right && c2->right){
+                q1.push(c1->right);
+                q2.push(c2->right);
+            }else if(c1->right || c2->right){
+                return 0;
+            }  
+        }
+        return q1.size() == q2.size();
+    }
+
     bool isSameTree(TreeNode* p, TreeNode* q) {
+        
+        if(p == NULL && q == NULL){
+            return 1;
+        }else if(p == NULL || q == NULL){
+            return 0;
+        }
+
         bool ans = false;
-        ans =  DFSSolution(p,q);
+        //ans =  DFSSolution(p,q);
+        //ans =  BFSSolution(p,q);
+
 
         return ans;
     }
