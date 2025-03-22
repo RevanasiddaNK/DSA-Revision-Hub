99b30f14742c3667a580c49372eab0cadfcd8160
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-03-22 23:01:36 +0530

    Time: 0 ms (100%), Space: 15.1 MB (66.41%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,8 +1,5 @@
 class Solution {
 public:
-
-
-
     int search(vector<int>& nums, int target) {
         int n = nums.size();
         int s=0, e=n-1;
@@ -12,26 +9,20 @@ public:
             if(nums[m] == target){
                 return m;
             }
-            else if( nums[s] <= nums[m] ){
-                if( nums[s] <= target && nums[m] >= target ){
-                     e = m-1;
+            else if(nums[s] <= nums[m] ){
+                if(nums[s] <= target && nums[m] >= target){
+                    e = m-1;
                 }else{
                     s = m+1;
                 }
-            }
-             else{
-                if( nums[m] <= target && nums[e] >= target ){
+            }else{
+                if(nums[m] <= target && nums[e] >= target){
                     s = m+1;
                 }else{
-                    e=m-1;
+                    e = m-1;
                 }
             }
         }
         return -1;
     }
 };
-/*
-1. find out where is mid lying ?
-2. if its lying in left part, check wether target lies in left part, if does consider left otherwise ignore right, same for right part too
-
-*/
