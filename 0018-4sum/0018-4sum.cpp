2fb06c73d2036d5c17c6ff37db96d24cd33dd73b
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-03-31 23:38:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>>ans;
+
         sort(nums.begin(), nums.end());
+        vector<vector<int>>ans;
         int n = nums.size();
-
-        for(int i=0; i<n; i++){
-
-           if(i > 0 && nums[i] == nums[i-1]) continue;
-          
-
+        for(int i=0; i<n; i++ ){
+            if(i>0 && nums[i] == nums[i-1])
+                    continue;
             for(int j=i+1; j<n; j++){
-
-               if(j > i + 1 && nums[j] == nums[j-1]) continue;
-              
-                int s=j+1, e = n-1;
+                if (j > i + 1 && nums[j] == nums[j - 1]) 
+                    continue;
+                int s=j+1, e=n-1;
                 while(s < e){
-                    double sum = nums[i]/4.0 + nums[j] /4.0+ nums[s]/4.0 + nums[e]/4.0;
-                    if(target/4.00 == sum){
-                        ans.push_back({nums[i] , nums[j] , nums[s] , nums[e]});
+                    long long sum = nums[i]+nums[j]+nums[s]+nums[e];
+                    if(sum == target){
+                        ans.push_back({nums[i],nums[j],nums[s],nums[e]});
+                        
+                        int st = nums[s++];
+                        int ed = nums[e--];
 
-                        int k = nums[s];
-                        while(s<e && k == nums[s]) 
+                        while(s<e && st == nums[s])
                             s++;
-                        k = nums[e];
-                        while(s<e && nums[e] == k)
-                            e--;
 
-                    }
-                    else if(sum < target/4.00)
+    
+                        while(s<e && ed == nums[e])
+                            e--;
+                        
+                    }else if(sum <= target)
                         s++;
                     else
                         e--;
-                }
-               
+                } 
             }
-        
         }
         return ans;
     }
