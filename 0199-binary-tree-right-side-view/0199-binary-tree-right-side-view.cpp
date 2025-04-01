ef466d45b8b1ee980e4eca92c4bbeff3f4b1bd84
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-04-01 23:16:28 +0530

    Time: 0 ms (100%), Space: 14.8 MB (84.75%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -12,6 +12,7 @@
 class Solution {
 public:
 
+//  1] Using Recursion – O(n) Time and O(n) Space
     void RecursiveRightView(TreeNode* root, int lvl, int &maxLevel, vector<int>&ans){
 
         if(!root)
@@ -29,11 +30,83 @@ public:
     }
 
 
+//  2] Using Level Order Traversal – O(n) Time and O(n) Space
+    void LevelRightView(TreeNode* root, vector<int>&ans){
+            if(!root)
+                return;
+            queue<TreeNode*>q;
+            q.push(root);
+            while(!q.empty()){
+                int n = q.size();
+                TreeNode* f = q.front();
+                q.pop();
+
+                if(f->right)
+                    q.push(f->right);
+                if(f->left)
+                    q.push(f->left);
+
+                ans.push_back(f->val);
+
+                for(int i=1; i<n; i++){
+
+                    TreeNode* f = q.front();
+                    q.pop();
+
+                    if(f->right)
+                        q.push(f->right);
+                    if(f->left)
+                        q.push(f->left);
+
+                }
+
+            }
+    }
+
+
+
+
+//  3] Using Morris Traversal – O(n) Time and O(1) Space
+    void MorrisRightView(TreeNode* root, vector<int>&res){
+       int level = 0;
+        while(root){
+            if(root->right){
+                TreeNode* pred = root->right;
+                int bd = 1;
+                while(pred->left != NULL && pred->left != root){
+                    pred = pred->left;
+                    bd++;
+                }
+
+                if(pred->left == NULL){
+                    if(level == res.size()){
+                        res.push_back(root->val);
+                    }
+                    pred->left = root;
+                    root = root->right;
+                    level++;
+                }
+                else{
+                    pred->left = NULL;
+                    level -= bd;
+                    root = root->left;
+                }
+            }else{
+                if(level == res.size()){
+                    res.push_back(root->val);
+                }
+                level++;
+                root = root->left;
+            }
+        }
+    }
+
     vector<int> rightSideView(TreeNode* root) {
         vector<int>ans;
         int maxLevel = -1;
-        RecursiveRightView(root, 0, maxLevel, ans);
-
+        //RecursiveRightView(root, 0, maxLevel, ans);
+        //LevelRightView(root, ans);
+        MorrisRightView(root, ans);
         return ans;
     }
 };
