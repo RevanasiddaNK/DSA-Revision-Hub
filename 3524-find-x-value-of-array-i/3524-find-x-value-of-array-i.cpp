45e6222d599a45f43577261056c34794e0439fd5
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-04-21 23:09:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<long long> resultArray(vector<int>& nums, int k) {
+        vector<long long>ans(k,0);
+        long long prod = 1;
         int n = nums.size();
-        vector<long long> ans(k, 0), cnt(k, 0);
-        for (int i = 0; i < n; i++) {
-            nums[i] %= k;
-            vector<long long> nCnt(k, 0);
-            nCnt[nums[i]]++;
-            for (int x = 0; x < k; x++) {
-                nCnt[x * nums[i] % k] += cnt[x];
+
+        for(int i=0; i<n; i++){
+            prod = 1;
+            for(int j=i; j<n; j++){
+               prod = (static_cast<__int128>(prod) * nums[j]) % k;
+                ans[ prod % k ]++;
             }
-            cnt = nCnt;
-            for (int x = 0; x < k; x++) ans[x] += cnt[x];
         }
         return ans;
     }
