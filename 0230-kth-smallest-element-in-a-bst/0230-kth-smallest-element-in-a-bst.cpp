6d1e1d0940a694f5897e3015c297b1eeb1881a4d
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-04-28 12:08:52 +0530

    Time: 0 ms (100%), Space: 24.3 MB (67.46%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -20,9 +20,29 @@ public:
         inorderTraversal(root->right, inorder);
     }
 
+    int inorderTraversal(TreeNode* root, int &k) {
+        if (!root)
+            return -1;
+       
+
+        int l = inorderTraversal(root->left, k);
+        k--;
+        if( k == 0)
+            return root->val;
+        int r = inorderTraversal(root->right, k);
+        if( l != -1)
+            return l;
+        if(r != -1)
+            return r;
+        return -1;
+    }
+
     int kthSmallest(TreeNode* root, int k) {
+        /*
         vector<int>inorder;
         inorderTraversal(root, inorder);
         return inorder[k-1];
+        */
+        return inorderTraversal(root,k);
     }
 };
