a3d0d31e11b735e2a94da56a3abf7d62134c4072
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-04-28 11:18:14 +0530

    Time: 0 ms (100%), Space: 22 MB (47.61%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -20,7 +20,7 @@ public:
         inorderTraversal(root->right,inorder );
     }
 
-    bool isValidBST(TreeNode* root) {
+    bool isValidBSTinorderTraversal(TreeNode* root) {
         vector<int>inorder;
         inorderTraversal(root,inorder );
         int n = inorder.size();
@@ -30,5 +30,26 @@ public:
             }
         }
         return 1;
+    }
+
+    bool solveMinMax( TreeNode* root,  long long min , long long max ){
+        if(!root)
+            return 1;
+       
+        if(min >= root->val || max <= root->val ){
+            return 0;
+        }
+
+        bool left = solveMinMax(root->left, min, root->val);
+        bool right = solveMinMax(root->right, root->val, max);
+        return right && left;
+    }
+
+    bool isValidBST(TreeNode* root) {
+        bool ans = false;
+        //ans = isValidBSTinorderTraversal(root);
+        ans = solveMinMax(root, LLONG_MIN, LLONG_MAX);
+
+        return ans;
     }
 };
