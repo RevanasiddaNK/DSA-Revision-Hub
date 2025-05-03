d08237ed3cf6daf55cc1766bd2318e4397a8b2e9
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-05-03 22:30:20 +0530

    Time: 0 ms (100%), Space: 8.4 MB (48.25%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,22 +1,44 @@
 class Solution {
 public:
 
-    int solve(int i,int n, vector<int>&dp){
-        if(i == n){
-            return 1;
+    int climbStairsMem(int n, vector<int>&dp) {
+        if(n <= 0)
+            return  n+1;
+        if(dp[n]  != -1)
+            return dp[n];
+        return  dp[n] = climbStairsMem(n-1, dp) + climbStairsMem(n-2, dp);
+    }
+
+    int solveTab(int n){
+        vector<int>dp(n+2, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<n+2; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        if(dp[i] != -1)
-            return dp[i];
-        int t = 0;
-        if(i+2 <= n)
-            t = solve(i+2,n,dp);
-        int o = solve(i+1,n,dp);
-        return dp[i] = (t+o);
+        return dp[n+1];
+    }
+
+    int solveSpaceOpt(int n){
+       
+        int prev1 = 0;
+        int prev2 = 1;
+        for(int i=2; i<n+2; i++){
+            int curr = prev1 + prev2 ;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return prev2;
     }
 
     int climbStairs(int n) {
-      
+        
+        if(n == 0)
+            return 0;
+
         vector<int>dp(n+1, -1);
-        return solve(0,n, dp);
+        //return climbStairsMem(n, dp);
+        //return solveTab(n);
+        return solveSpaceOpt(n);
     }
 };
