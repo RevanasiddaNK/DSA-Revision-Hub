ffe7e205645419a1b686fa9ee9bf08ef21770a12
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-05-04 22:53:34 +0530

    Time: 0 ms (100%), Space: 17.3 MB (5.93%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -83,9 +83,104 @@ public:
         head->next = recSol(curr, k);
         return prev;
     }
+/*
+    ListNode*  stackSol(ListNode* head, int k){
+        ListNode* nh = NULL;
+        ListNode* curr = head;
+        ListNode* tail = NULL;
+        while(curr){
+            ListNode* check = curr;
+            int count = 0;
+            while (check && count < k) {
+                check = check->next;
+                count++;
+            }
+
+            if (count < k) {
+                if (tail) tail->next = curr;
+                break;
+            }
+
+            ListNode* grphead = curr;
+            int cnt = 0;
+            stack<ListNode*>st;
+
+            while(curr && cnt < k ){
+                st.push(curr);
+                curr = curr->next;
+                cnt++;
+            }
+            ListNode* nh1 = st.top();
+            if(!nh){
+                nh = nh1;
+            }
+
+            ListNode* gtail = st.top();
+            st.pop();
+            while(!st.empty()){
+                gtail->next = st.top();
+                st.pop();
+            }
+
+            if(tail){
+                tail->next = nh1;
+            }
+            tail = gtail;
+            curr =  gtail->next;
+        }
+        return nh;
+    }
+*/
+    ListNode* stackSol(ListNode* head, int k) {
+    ListNode* nh = NULL;
+    ListNode* curr = head;
+    ListNode* tail = NULL;
+
+    while (curr) {
+        ListNode* check = curr;
+        int count = 0;
+        while (check && count < k) {
+            check = check->next;
+            count++;
+        }
+
+        if (count < k) {
+            if (tail) tail->next = curr;
+            break;
+        }
+
+        stack<ListNode*> st;
+        ListNode* temp = curr;
+        for (int i = 0; i < k; i++) {
+            st.push(temp);
+            temp = temp->next;
+        }
+
+        ListNode* newGroupHead = st.top();
+        st.pop();
+        ListNode* prev = newGroupHead;
+        while (!st.empty()) {
+            prev->next = st.top();
+            st.pop();
+            prev = prev->next;
+        }
+
+        prev->next = temp;  // ✅ Important fix: Connect group tail to next
+
+        if (!nh) nh = newGroupHead;
+        if (tail) tail->next = newGroupHead;
+
+        tail = prev;
+        curr = temp;
+    }
+
+    return nh;
+}
+
 
     ListNode* reverseKGroup(ListNode* head, int k) {
         //return reverseKGroupIterativeMethod(head,k);
-        return recSol(head,k);
+        // return recSol(head,k);
+        return stackSol(head,k);
     }
 };
