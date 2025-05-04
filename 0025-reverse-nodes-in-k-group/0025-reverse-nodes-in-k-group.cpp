45d35d80f0147c8a1279662810709d8a7a6d920b
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-05-04 22:53:22 +0530

    Time: 0 ms (100%), Space: 16.4 MB (70.29%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,7 +10,8 @@
  */
 class Solution {
 public:
-    ListNode* reverseKGroup(ListNode* head, int k) {
+
+    ListNode* reverseKGroupIterativeMethod(ListNode* head, int k) {
         ListNode* nh = NULL;
         ListNode* curr = head;
         ListNode* tail = NULL;
@@ -51,5 +52,40 @@ public:
             tail = grphead;
         }
         return nh;
+    }
+
+    ListNode* recSol(ListNode* head, int k){
+        if(!head){
+            return head;
+        }
+        
+        ListNode* check = head;
+        int cnt = 0;
+        while(check && cnt < k){
+            check = check->next;
+            cnt++;
+        }
+        if(cnt < k)
+            return head;
+        
+        // revrse k nodes
+        ListNode* curr = head;
+        ListNode* next = head->next;
+        ListNode* prev = NULL;
+        cnt = 0;
+        while(curr && cnt < k){
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+            cnt++;
+        }
+        head->next = recSol(curr, k);
+        return prev;
+    }
+
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        //return reverseKGroupIterativeMethod(head,k);
+        return recSol(head,k);
     }
 };
