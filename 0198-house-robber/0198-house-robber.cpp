b05d1eaf8644295aba395c214793463399c47e0d
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-05-09 19:51:08 +0530

    Time: 0 ms (100%), Space: 10.7 MB (22.24%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -14,9 +14,22 @@ public:
         return dp[i] = max(inc, exc);
     }
 
+    int solveTab(vector<int>& nums, int n, vector<int>&dp){
+
+        dp[n] = 0;
+        dp[n+1] = 0;
+        for(int i=n-1; i>=0; i--){
+            int inc = nums[i] + dp[i+2];
+            int exc = dp[i+1];
+            dp[i] = max(inc, exc);
+        }
+        return dp[0];
+    }
+
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n+1, -1);
-        return solveMem(0, nums, n, dp);
+        vector<int>dp(n+2, -1);
+        //return solveMem(0, nums, n, dp);
+        return solveTab( nums, n, dp);
     }
 };
