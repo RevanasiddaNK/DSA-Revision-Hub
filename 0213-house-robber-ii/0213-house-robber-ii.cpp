198b9a5e3d10566e44ec12938c50a18b49570b99
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-05-09 21:58:40 +0530

    Time: 0 ms (100%), Space: 10.9 MB (40.19%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,33 +1,48 @@
 class Solution {
 public:
+    // ------------------ Memoization ------------------
+    int solveMem(int i, int end, vector<int>& nums, vector<int>& dp) {
+        if (i > end) return 0;
+        if (dp[i] != -1) return dp[i];
 
-     int solveMem( int i, vector<int>& nums, int n, vector<int>&dp){
+        int include = nums[i] + solveMem(i + 2, end, nums, dp);
+        int exclude = solveMem(i + 1, end, nums, dp);
 
-        if(i >= n)
-            return 0;
-        
-        if(dp[i] != -1)
-            return dp[i];
-        
-        int inc = nums[i] + solveMem(i+2, nums, n, dp);
-        int exc = solveMem(i+1, nums, n, dp);
-        return dp[i] = max(inc, exc);
+        return dp[i] = max(include, exclude);
     }
 
+    // ------------------ Tabulation ------------------
+    int solveTab(int start, int end, vector<int>& nums) {
+        int len = end - start + 1;
+        vector<int> dp(len + 2, 0);
 
+        for (int i = end; i >= start; i--) {
+            int idx = i - start;
+            int include = nums[i] + dp[idx + 2];
+            int exclude = dp[idx + 1];
+            dp[idx] = max(include, exclude);
+        }
 
+        return dp[0];
+    }
+
+    // ------------------ Main Function ------------------
     int rob(vector<int>& nums) {
         int n = nums.size();
+        if (n == 1) return nums[0];
 
-        if(n == 1)
-            return nums[0];
+        // ---------- Memoization ----------
+        vector<int> dp1(n, -1);
+        vector<int> dp2(n, -1);
+ /*       
+        int option1 = solveMem(0, n - 2, nums, dp1); // exclude last
+        int option2 = solveMem(1, n - 1, nums, dp2); // exclude first
+*/
 
-        vector<int>dp1(n+1, -1);
-        int option1 = solveMem( 0, nums, n-1, dp1);
+        // ---------- Tabulation (alternative to memo) ----------
+        int option1 = solveTab(0, n - 2, nums);
+        int option2 = solveTab(1, n - 1, nums);
 
-        vector<int>dp2(n+1, -1);
-        int option2 = solveMem( 1, nums, n, dp2);
         return max(option1, option2);
-        
     }
 };
