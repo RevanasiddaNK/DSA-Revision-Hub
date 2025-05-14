df3dbe13aeec0dcf7f27a02351d6e7c7bf7a53d9
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-05-14 09:49:29 +0530

    Time: 0 ms (100%), Space: 10.8 MB (55.27%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // ------------------ Memoization ------------------
+    
     int solveMem(int i, int end, vector<int>& nums, vector<int>& dp) {
         if (i > end) return 0;
         if (dp[i] != -1) return dp[i];
@@ -11,62 +11,43 @@ public:
         return dp[i] = max(include, exclude);
     }
 
-    // ------------------ Tabulation ------------------
-    int solveTab(int start, int end, vector<int>& nums) {
-        int len = end - start + 1;
-        vector<int> dp(len + 2, 0);
+    int solveTab(int s, int e,  vector<int>& nums){
 
-        for (int i = end; i >= start; i--) {
-            int idx = i - start;
-            int include = nums[i] + dp[idx + 2];
-            int exclude = dp[idx + 1];
-            dp[idx] = max(include, exclude);
+        int n = e-s+1;
+        vector<int>dp(n+2, 0);
+    
+        for(int i=e; i>=s; i--){
+
+            int index = i-s;
+            int include = nums[i] + dp[index + 2];
+            int exclude = dp[index+1];
+            dp[index] = max(include, exclude);
+
         }
-
         return dp[0];
     }
-
-    int solveSpaceOpt(int start, int end, vector<int>& nums) {
-    int next1 = 0; // dp[i+1]
-    int next2 = 0; // dp[i+2]
-    int curr = 0;
-
-    for (int i = end; i >= start; i--) {
-        int include = nums[i] + next2;
-        int exclude = next1;
-        curr = max(include, exclude);
-
-        // shift window
-        next2 = next1;
-        next1 = curr;
-    }
-
-    return curr;
-}
-
 
     // ------------------ Main Function ------------------
     int rob(vector<int>& nums) {
         int n = nums.size();
         if (n == 1) return nums[0];
 
-        // ---------- Memoization ----------
+/*
         vector<int> dp1(n, -1);
         vector<int> dp2(n, -1);
- /*       
         int option1 = solveMem(0, n - 2, nums, dp1); // exclude last
         int option2 = solveMem(1, n - 1, nums, dp2); // exclude first
 */
 
-/*
+
         // ---------- Tabulation (alternative to memo) ----------
         int option1 = solveTab(0, n - 2, nums);
         int option2 = solveTab(1, n - 1, nums);
-*/
 
+/*
         int option1 = solveSpaceOpt(0, n - 2, nums); // exclude last
         int option2 = solveSpaceOpt(1, n - 1, nums); // exclude first
-
+*/
         return max(option1, option2);
     }
 };
