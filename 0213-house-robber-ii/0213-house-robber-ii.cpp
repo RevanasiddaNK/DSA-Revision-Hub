05717347d124b5ab6c2331ef566576d178d52d3e
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-05-14 09:44:20 +0530

    Time: 0 ms (100%), Space: 10.9 MB (39.94%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -26,6 +26,25 @@ public:
         return dp[0];
     }
 
+    int solveSpaceOpt(int start, int end, vector<int>& nums) {
+    int next1 = 0; // dp[i+1]
+    int next2 = 0; // dp[i+2]
+    int curr = 0;
+
+    for (int i = end; i >= start; i--) {
+        int include = nums[i] + next2;
+        int exclude = next1;
+        curr = max(include, exclude);
+
+        // shift window
+        next2 = next1;
+        next1 = curr;
+    }
+
+    return curr;
+}
+
+
     // ------------------ Main Function ------------------
     int rob(vector<int>& nums) {
         int n = nums.size();
@@ -39,9 +58,14 @@ public:
         int option2 = solveMem(1, n - 1, nums, dp2); // exclude first
 */
 
+/*
         // ---------- Tabulation (alternative to memo) ----------
         int option1 = solveTab(0, n - 2, nums);
         int option2 = solveTab(1, n - 1, nums);
+*/
+
+        int option1 = solveSpaceOpt(0, n - 2, nums); // exclude last
+        int option2 = solveSpaceOpt(1, n - 1, nums); // exclude first
 
         return max(option1, option2);
     }
