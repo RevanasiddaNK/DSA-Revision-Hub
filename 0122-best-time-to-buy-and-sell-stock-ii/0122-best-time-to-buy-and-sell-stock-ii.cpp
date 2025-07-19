c31f14a52947ca0167a81fb68431cad47491a818
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-07-19 17:28:09 +0530

    Time: 5 ms (28.16%), Space: 22.2 MB (14.51%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,88 +1,31 @@
 class Solution {
 public:
 
-    int solveMem(vector<int>& prices, int i, int buy, vector< vector<int>>&dp ){
-        if(i >= prices.size())
+    int recSol(int i, bool buy, vector<int>& prices, vector<vector<int>>&dp){
+
+        if(i < 0)
             return 0;
 
-        if(dp[i][buy] != INT_MIN)
+        if(dp[i][buy] != -1)
             return dp[i][buy];
 
-        int profit = 0;
+        int inc=0, exc=0;
         if(buy){
-            int inc = -prices[i] + solveMem(prices,i+1,0,dp);
-            int exc = solveMem(prices,i+1,1,dp);
-            profit = max(inc, exc);
+            inc = recSol( i-1, 0, prices , dp) - prices[i];
+            exc = recSol( i-1, 1, prices, dp );
+        }else{
+            inc = recSol( i-1, 1, prices, dp ) + prices[i];
+            exc = recSol( i-1, 0, prices, dp );
         }
-        else{
-            int inc = prices[i] + solveMem(prices,i+1,1,dp);
-            int exc = solveMem(prices,i+1,0,dp);
-            profit = max(inc, exc);
-        }
-        return dp[i][buy] = profit;
-    }
 
-    int solveTab(vector<int>& prices,int n){
-        vector< vector<int> >dp(n+1, vector<int>(2,0));
-
-        for(int i=n-1; i>=0; i--){
-            for(int b=0; b<2; b++){
-                int profit = 0;
-                if(b){
-                    int inc = -prices[i] + dp[i+1][0];
-                    int exc = dp[i+1][1];
-                    profit = max(inc, exc);
-                }
-                else{
-                    int inc = prices[i] + dp[i+1][1];
-                    int exc = dp[i+1][0];
-                    profit = max(inc, exc);
-                }
-
-                dp[i][b] = profit;
-            }
-        }
-        return dp[0][1];
-    }
-
-     int solveSpaceOpt(vector<int>& prices,int n){
-        
-        vector<int>dp(2,0);
-        for(int i=n-1; i>=0; i--){
-          
-            for(int b=0; b<2; b++){
-                int profit = 0;
-                if(b){
-                    int inc = -prices[i] + dp[0];
-                    int exc = dp[1];
-                    profit = max(inc, exc);
-                }
-                else{
-                    int inc = prices[i] + dp[1];
-                    int exc = dp[0];
-                    profit = max(inc, exc);
-                }
-
-                dp[b] = profit;
-            }
-        }
-        return dp[1];
+        return dp[i][buy] = max(inc, exc);
     }
 
     int maxProfit(vector<int>& prices) {
+        bool buy = 1;
         int n = prices.size();
-      
-        vector< vector<int> >dp(n+1, vector<int>(2, INT_MIN));
-        //return solveMem(prices,0,1,dp);
-
-        //return solveTab(prices, n);
-
-        return solveSpaceOpt(prices, n);
-
+        reverse(prices.begin(), prices.end());
+        vector<vector<int>>dp(n, vector<int>(2,-1));
+        return recSol( n-1, buy, prices, dp );
     }
 };
-
-/*
-TC : O(N)
-SC : O(N)
-*/
