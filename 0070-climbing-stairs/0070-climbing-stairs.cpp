72e00be3387e37745e3ca8e4cd3cd758d7df40cc
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-08-15 23:03:53 +0530

    Time: 0 ms (100%), Space: 8.5 MB (52.24%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,44 +1,19 @@
 class Solution {
 public:
 
-    int climbStairsMem(int n, vector<int>&dp) {
+    int solveMem(int n, vector<int>&dp){
         if(n <= 0)
-            return  n+1;
-        if(dp[n]  != -1)
+            return n+1;
+        if(dp[n] != -1 ){
             return dp[n];
-        return  dp[n] = climbStairsMem(n-1, dp) + climbStairsMem(n-2, dp);
+        }
+        int cnt =  solveMem(n-1, dp) + solveMem(n-2, dp);
+        return dp[n]= cnt;
     }
 
-    int solveTab(int n){
-        vector<int>dp(n+2, -1);
-        dp[0] = 0;
-        dp[1] = 1;
-        for(int i=2; i<n+2; i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n+1];
-    }
-
-    int solveSpaceOpt(int n){
-       
-        int prev1 = 0;
-        int prev2 = 1;
-        for(int i=2; i<n+2; i++){
-            int curr = prev1 + prev2 ;
-            prev1 = prev2;
-            prev2 = curr;
-        }
-        return prev2;
-    }
 
     int climbStairs(int n) {
-
-        if(n == 0)
-            return 0;
-
-        vector<int>dp(n+1, -1);
-        //return climbStairsMem(n, dp);
-        return solveTab(n);
-        return solveSpaceOpt(n);
+        vector<int>dp(n+1,-1);
+        return solveMem(n, dp);
     }
 };
