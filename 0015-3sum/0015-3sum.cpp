fde615a26d2b1073cc76ee77d2531d943ef5546f
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-08-25 20:08:37 +0530

    Time: 47 ms (70.04%), Space: 29.2 MB (48.26%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,31 +3,40 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>>ans;
         sort(nums.begin(), nums.end());
+
         int n = nums.size();
         for(int i=0; i<n; i++){
 
-            if(i>0 && nums[i] == nums[i-1])
+            int s=i+1, e=n-1;
+            if( i>0 && nums[i] == nums[i-1] )
                 continue;
-            int s=i+1,e=n-1;
-            while(s < e){
-                double sum = nums[s] + nums[e] + nums[i];
-                if(sum == 0){
-                    ans.push_back({ nums[i], nums[s], nums[e]  });
-                    int st = nums[s], ed = nums[e];
-                    s++;
-                    e--;
-                    while(s < e && st == nums[s])
-                        s++;
-                    while(s < e && ed == nums[e])
-                        e--;
-                    
 
-                }else if(sum < 0){
+            while(s < e){
+
+                int sum = nums[i] + nums[s] + nums[e];
+
+                if(sum == 0){
+
+                    ans.push_back( {nums[i], nums[s], nums[e]  } );
+                    int sEl = nums[s], eEl = nums[e];
                     s++;
-                }else{
                     e--;
+                    while( s<e && nums[s] == sEl ){
+                        s++;
+                    }
+
+                    while( s<e && nums[e] == eEl ){
+                        e--;
+                    }
+
                 }
+                else if(sum < 0)
+                    s++;
+                else
+                    e--;
+
             }
+
         }
         return ans;
     }
