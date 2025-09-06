c0985872df68f4a188ba86d03bc75c75fa9afb7d
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2025-09-06 19:46:14 +0530

    Time: 0 ms (100%), Space: 8.7 MB (18.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
 
-    int solveMem(int n, vector<int>&dp){
-        if(n <= 0)
-            return n+1;
-        if(dp[n] != -1 ){
-            return dp[n];
-        }
-        int cnt =  solveMem(n-1, dp) + solveMem(n-2, dp);
-        return dp[n]= cnt;
+    int solveMem(int i, int n, vector<int>&dp){
+
+        if(i >= n)
+            return i == n;
+        
+        if(dp[i] != -1)
+            return dp[i];
+        
+        int one = solveMem(i+1, n, dp);
+        int two = solveMem(i+2, n, dp);
+
+        return dp[i] = one+two;
+    
     }
 
-
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return solveMem(n, dp);
+        vector<int>dp(n+1, -1);
+        return solveMem(0, n, dp);
     }
 };
